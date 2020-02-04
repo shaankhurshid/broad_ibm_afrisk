e360c9f6d4fa6937966dac7f624342624d2c5880
Shaan Khurshid <sk259@da01.research.partners.org>
2020-02-03 20:01:29 -0500

    subgroup plot

--- subgroup_plot.R
+++ subgroup_plot.R
@@ -1,30 +1,38 @@
 # Dependenices
 library(data.table)
 
-# Load output file with hand-curated disease names (OPTIONAL)
-hf <- fread('/data/arrhythmia/skhurshid/broad_ibm_afrisk/hf_subgroup.csv')
-stroke <- fread('/data/arrhythmia/skhurshid/broad_ibm_afrisk/stroke_subgroup.csv')
+# Load subgroup files
+f <- fread('/data/arrhythmia/skhurshid/broad_ibm_afrisk/female.csv')
+m <- fread('/data/arrhythmia/skhurshid/broad_ibm_afrisk/male.csv')
+hf <- fread('/data/arrhythmia/skhurshid/broad_ibm_afrisk/hf.csv')
+stroke <- fread('/data/arrhythmia/skhurshid/broad_ibm_afrisk/stroke.csv')
 
-# Sort data by concordance (low to high)
-output <- output[order(output$concordance),]
+# Create all
+all <- data.frame(Score_Name='EHR_AF_Final',Low_CI=0.780,CI_value=0.781,High_CI=0.782,N_outcome=152205,
+                  N_selected_population=4510814,Outcome_inc=NA,N_total=4510814)
 
-png('/data/arrhythmia/skhurshid/broad_ibm_afrisk/concordance_ccs1.png',height=500,width=800)
+# Collapse EHR_AF results only
+ehr <- rbind(f[Score_Name=='EHR_AF_Final'],m[Score_Name=='EHR_AF_Final'],
+             stroke[Score_Name=='EHR_AF_Final'],hf[Score_Name=='EHR_AF_Final'],all[1,])
+ehr$subgroup <- c('Females','Males','Stroke','Heart failure','Overall')
+
+# Plot
+png('/data/arrhythmia/skhurshid/broad_ibm_afrisk/subgroup.png',height=350,width=800,res=100)
 par(oma=c(1,1,1,1))
-par(mar=c(3,32,1,1))
-plot(x=output$concordance,y=seq(1,18,1),xlim=c(0.700,1.00),
-     xaxt='n',yaxt='n',xlab='',ylab='',pch=19,col='#6baed6',cex=1)
-axis(1,at=seq(0.700,1.00,0.025),cex=1.4)
+par(mar=c(3,19,1,1))
+col <- c('#e31a1c','#1f78b4','#33a02c','#ff7f00','black')
+plot(x=ehr$CI_value,y=seq(5,1,-1),xlim=c(0.600,0.820),ylim=c(0.75,5.25),
+     xaxt='n',yaxt='n',xlab='',ylab='',pch=19,col=col,cex=1.4)
+axis(1,at=seq(0.600,0.820,0.020),cex=1.4)
 axis(2,at=1:18,labels=FALSE,cex=1.4)
-mtext('CCS class (level 1)',side=2,line=31.5,cex=1.3)
+mtext('Subgroup',side=2,line=18.5,cex=1.3)
 mtext('Concordance',side=1,line=2.5,cex=1.3)
-segments(output$concordance_lb,1:18,output$concordance_ub,1:18,col='#6baed6',lwd=2.2)
+segments(ehr$Low_CI,5:1,ehr$High_CI,5:1,col=col,lwd=2.2)
 
-plot_names <- output$ccs1
+plot_names <- c('overall','heart failure','stroke','male','female')
 for (i in 1:length(plot_names)){
-  mtext(paste0(plot_names[i],' (N=',output$n_disease[i],'; AF=',output$incd_af[i],')'),
+  mtext(paste0(plot_names[i],' (N=',ehr$N_selected_population[i],'; AF=',ehr$N_outcome[i],')'),
         side=2,line=1,las=2,at=i)
 }
 
-text(output$concordance_ub+0.03,1:17,as.character(round(output$concordance,3)))
-text(output$concordance_lb[18]-0.03,18,as.character(round(output$concordance[18],3)))
 dev.off()
