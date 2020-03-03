77a4b35392696bebe311cabe0094a834b99429f2
shaankhurshid <56035798+shaankhurshid@users.noreply.github.com>
2020-03-03 13:59:37 -0500

    Update density_combined.R

--- density_combined.R
+++ density_combined.R
@@ -11,12 +11,12 @@ no_incident_af <- mydata[is_AFib_combined==0,]
 
 ##### PLOT 1: CHADS
 controls <- count(no_incident_af$pred_risk_CHA2DS2_VASc_Final)
-controls_probs <- controls[,2] / nrow(controls)
+controls_probs <- controls[,2] / nrow(no_incident_af)
 
 cases <- count(incident_af$pred_risk_CHA2DS2_VASc_Final)
-cases_probs <- cases[,2] / nrow(cases)
+cases_probs <- cases[,2] / nrow(incident_af)
 
-x <- factor(0:(length(cases_probs)-1))
+x <- factor(cases[,1])
 y1 <- controls_probs
 y2 <- cases_probs
 
@@ -25,7 +25,7 @@ melted <- melt(data,id="x")
 
 # Plot
 ggplot(melted,aes(x=x,y=value,fill=variable)) + geom_bar(stat='identity',position='identity',alpha=0.55,width=1,color='black') +
-  scale_x_discrete(breaks=seq(0,length(x)-1,1),expand=c(0,0),name=expression(paste('   ',CHA[2],DS[2],-VASc,' ',Risk))) +
+  scale_x_discrete(breaks=seq_along(x),expand=c(0,0),name=expression(paste('   ',CHA[2],DS[2],-VASc,' ',Risk))) +
   scale_y_continuous(breaks=seq(0,0.3,0.05),expand=c(0,0),limits=c(0,0.30),name='frequency') +
   scale_fill_manual(values=c("#2b8cbe","#f03b20"),name='',labels=c('No AF','AF')) +
   theme(panel.background=element_blank(),axis.line=element_line(color='black'),legend.position=c(0.85,0.90),
@@ -41,7 +41,7 @@ controls_probs <- controls[,2] / nrow(controls)
 cases <- count(incident_af$pred_risk_C2HEST_Final)
 cases_probs <- cases[,2] / nrow(cases)
 
-x <- factor(0:(length(cases_probs)-1)) 
+x <- factor(cases[,1])
 y1 <- controls_probs
 y2 <- cases_probs
 
@@ -50,7 +50,7 @@ melted <- melt(data,id="x")
 
 # Plot
 ggplot(melted,aes(x=x,y=value,fill=variable)) + geom_bar(stat='identity',position='identity',alpha=0.55,width=1,color='black') +
-  scale_x_discrete(breaks=seq(0,length(x)-1,1),expand=c(0,0),name=expression(paste('   ',C[2],HEST,' ',Risk))) +
+  scale_x_discrete(breaks=seq_along(x),expand=c(0,0),name=expression(paste('   ',C[2],HEST,' ',Risk))) +
   scale_y_continuous(breaks=seq(0,0.3,0.05),expand=c(0,0),limits=c(0,0.30),name='frequency') +
   scale_fill_manual(values=c("#2b8cbe","#f03b20"),name='',labels=c('No AF','AF')) +
   theme(panel.background=element_blank(),axis.line=element_line(color='black'),legend.position=c(0.85,0.90),
