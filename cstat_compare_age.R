14620dcb58ebf6f9808d5e8ae678fe5dc12c6caf
Shaan Khurshid <sk259@da01.research.partners.org>
2020-03-26 17:23:56 -0400

    script to graph c-stats

---
+++ cstat_compare_age.R
@@ -0,0 +1,27 @@
+# Plot c-stat (corrected/uncorrected) by subgroups of age
+
+# Dependencies
+library(data.table)
+
+# Load data
+compare <- fread(file='/data/arrhythmia/skhurshid/broad_ibm_afrisk/cstat_compare.csv')
+
+# Plot
+pdf(file='/data/arrhythmia/skhurshid/broad_ibm_afrisk/cstat_compare.pdf',height=3,width=4,pointsize=4)
+x <- 1:nrow(compare)
+y1 <- compare$c_stat
+y2 <- compare$rederived_cstat
+
+plot(x,y1,xaxt='n',xlab='',yaxt='n',ylab='',col='#f46d43',pch=19,bty='n',ylim=c(0.480,0.800))
+par(new=TRUE)
+plot(x,y2,xaxt='n',xlab='',yaxt='n',ylab='',col='#1a9850',pch=19,bty='n',ylim=c(0.480,0.800))
+
+axis(1,at=x,labels=compare$age_class)
+axis(2,at=seq(0.480,0.800,0.02))
+
+mtext("age subgroup",1,line=3)
+mtext("c-statistic",2,line=3)
+
+legend(x=length(x)-3,y=0.80,c("original c-stat","rederived c-stat"),col=c('#f46d43','#1a9850'),bty='n',pch=19)
+
+dev.off()
